b056bd3a3a3fff0129eec5655b8cd62478d9372a
Aarthi Gk <aarthigk@in.ibm.com>
2021-12-12 19:08:18 +0530

    first commit

---
+++ src/main/java/com/techarchive/cucumberReporting/App.java
@@ -0,0 +1,13 @@
+package com.techarchive.cucumberReporting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
